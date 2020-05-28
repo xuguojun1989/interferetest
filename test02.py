91016465079eb999166849bcf0f5c735729ade06
gusiyuan <gu.siyuan@outlook.com>
2020-05-28 23:56:02 +0800

    salt加盐算法成功

--- test02.py
+++ test02.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
+from gs_sign import generateSignGetbx
 
 import requests
 
-url = "http://gatewaytest.gaosiedu.com/reg/api/30/Student/LoginByPassword"
+url = "http://gatewaytest.gaosiedu.com/reg/api/Util/Salt"
 
-data = {
-    "Phone": "13387836885",
-    "Password": "ada830094c2de643a8338da9aec82269",
-    "ClientId": "10000"
+params = {
+    "name": "13387836885"
+
 }
+sign = generateSignGetbx(params=params, bxEvn="gaosiedu", partner="10016")
 headers = {
     "Accept": "*/*",
     "partner": "10016",
@@ -16,10 +17,12 @@ headers = {
     "os": "iOS:13.3.1%20iPhone%206s%20WIFI",
     "winClientVersion": "3.1.0",
     "Accept-Language": "zh-Hans-CN;q=1",
-    "prefreshtoken":"",
-    "User-Agent":"gao si jiao yu/3.1.0 (iPhone; iOS 13.3.1; Scale/2.00)",
-
+    "prefreshtoken": "",
+    "User-Agent": "gao si jiao yu/3.1.0 (iPhone; iOS 13.3.1; Scale/2.00)",
+    "sign": sign
 
 }
 
-res = requests.post(url=url, json=data, headers="")
+res = requests.get(url=url, params=params, headers=headers)
+print(res.status_code)
+print(res.json())
